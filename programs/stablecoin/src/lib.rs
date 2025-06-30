b3fb25e03a024d7b6ce423b3036bebeb9b3ee73d
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-06-30 04:27:37 +0000

    init and added states for stablecoin

---
+++ programs/stablecoin/src/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+mod state;
+
+declare_id!("He6UwBtt2bHyi4EsvYMSq6BrepQFWxMdSircNLkNij7w");
+
+#[program]
+pub mod stablecoin {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
